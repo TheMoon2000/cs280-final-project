292a80ccd3be611ddb621f5396da69edc613e548
सागर संघवी <ssanghavi404@berkeley.edu>
2023-04-19 21:52:41 -0700

    junky

--- dataloaders/ImageNet.py
+++ dataloaders/ImageNet.py
@@ -8,7 +8,7 @@ class ImageNet(Dataset):
     def __init__(self) -> None:
         super().__init__()
 
-        self.filenames = glob.glob('/data/jerryshan/imagenet_val/*/*.JPEG')
+        self.filenames = glob.glob('/Users/sagarasanghavi/Desktop/Photos/*.jpg')
     
     def __len__(self):
         return len(self.filenames)
@@ -16,5 +16,5 @@ class ImageNet(Dataset):
     def __getitem__(self, index):
         original = cv2.imread(self.filenames[index]) # (H, W, 3)
         gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-        return gray, original.transpose((1, 2, 0))
+        return gray[None], original.transpose((2, 0, 1))
     
